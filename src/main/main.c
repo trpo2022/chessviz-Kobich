96d1a434022ca8d926298540d8556b9877774016
Kobich <kopanov_03@mail.ru>
2022-03-30 21:06:06 +0700

    new code

--- src/main/main.c
+++ src/main/main.c
@@ -3,17 +3,17 @@
 #include <stdio.h>
 int main()
 {
-    char mass[9][9];
-    int MoveNumber = 1;
-    int flag = 1;
-    CreateBoard(mass);
-    PrintBoard(mass);
-    while (flag == 1) {
-        printf("%d. ", MoveNumber);
-        WhiteTurn(mass);
-        BlackTurn(mass);
-        PrintBoard(mass);
-        MoveNumber += 1;
-    }
-    return 0;
+	char mass[9][9];
+	int MoveNumber = 1;
+	int j = 1;
+	CreateBoard(mass);
+	PrintBoard(mass);
+	while (j == 1) {
+		printf("%d.", MoveNumber);
+		WhiteTurn(mass);
+		PrintBoard(mass);
+		BlackTurn(mass);
+		PrintBoard(mass);
+		MoveNumber += 1;
+	}
 }
